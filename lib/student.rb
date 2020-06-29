9fd6e19f0e03ccd106db2f17e3b87a7812e990fd
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-29 00:28:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,43 +3,46 @@
 class Student
   
   attr_accessor :name, :grade
-  attr_reader :id 
-  
-  def initialize(name, grade, id=nil)
-    @id = id 
+  attr_reader :id
+
+  def initialize (name, grade, id = nil)
     @name = name
     @grade = grade
-  end 
-  
+    @id = id
+  end
+
   def self.create_table
-    sql = <<-SQL 
-    CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER)
-    SQL 
-    DB[:conn].execute(sql)
-  end 
-  
-  def self.drop_table
-    sql = <<-SQL 
-    DROP TABLE students
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        grade INTEGER
+        )
     SQL
-    DB[:conn].execute(sql)
-  end 
-  
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)  
+  end
+
   def save
     sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-    SQL 
-    DB[:conn].execute(sql, self.name, self.grade)
-  end 
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
 
-def self.create (name:, grade:)
-student = Student.new(name, grade)
-student.save
-student 
-end 
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
 end
